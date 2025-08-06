07039a00224e909a21dad90498f533f87920674d
Pedro-H13 <pedro.h.magalhaes.m@gmail.com>
2025-08-06 17:43:23 -0300

    060825

---
+++ atividades/atividade01/src/com/atividade01/app/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+            // todo 
+            /* */
+            *crie um programa que receba o nome, peso e altura,
+            *e calcule o valor do seu ICM (indice de masa corporal)
+            *cuja formula e peso/altura*altura, e infor,e o seu diagnostco
+            *baseado na tabela do IMC
+    }
+}
