03ee09261f9b1f62921da63ee82fc90f6c8eae23
Pedro-13 <pedro.h.magalhaes.m@gmail.com>
2025-08-07 17:44:39 -0300

    mensagem

--- atividades/atividade01/src/com/atividade01/app/App.java
+++ atividades/atividade01/src/com/atividade01/app/App.java
@@ -2,30 +2,48 @@ package com.atividade01.app;
 import java.util.Scanner;
 
 public class App {
-    
-
 
     public static void main(String[] args) throws Exception {
 
         Scanner leia = new Scanner(System.in);
 
-    
-        double peso,altura;
+        double peso,altura,resultado;
         String nome;
-    
-        System.out.print("informe o nome");
+    int opcao=0;
+        
+// verifificar o peso e altura
+do { 
+    System.out.println("1-novo calculo");
+    System.out.println("2-sair");
+    opcao=leia.nextInt();
+
+    if (opcao ==1){
+        leia.nextLine();
+        System.out.print("informe o nome: ");
         nome = leia.nextLine();
         System.out.print("informe o peso");
         peso = leia.nextDouble();
         System.out.print("informe a altura");
         altura = leia.nextDouble();
+         resultado = peso/(altura*altura);
         
-// verifificar o peso e altura
 
-
-
+    if (resultado< 18.5) {
+    System.out.println("condicao atual magreza, seu imc e; "+ resultado);
+} else if(resultado >= 18.5 && resultado<=24.9){
+     System.out.println("condicao atual normal, seu imc e; "+ resultado);
+} else if(resultado >= 25 && resultado<=29.9){
+     System.out.println("condicao atual sobrepeco, seu imc e; "+ resultado);
+}else if(resultado >= 30 && resultado<=39.9){
+     System.out.println("condicao atual obecidade, seu imc e; "+ resultado);
+}else if(resultado > 40){
+     System.out.println("condicao atuaL obecidade grave, seu imc e; "+ resultado);
+}
+    }
+    // todo 
+    
+}while (opcao !=2);
         }
-        
-
         }
        
+
--- atividades/atividade02/src/com/atividade02/app/App.java
+++ atividades/atividade02/src/com/atividade02/app/App.java
@@ -1,5 +1,41 @@
 package com.atividade02.app;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception    }
+    public static void main(String[] args) throws Exception  {
+
+Scanner leia = new Scanner(System.in);
+
+double idade =0;
+String nome;
+int opcao=0;
+System.out.print("informe o nome: ");
+        nome = leia.nextLine();
+        System.out.print("informe o idade");
+        idade = leia.nextDouble();
+// verificar idade
+do{
+    System.out.println("1 dracula. (+16)");
+    System.out.println("2 super man (+10)");
+    System.out.println("3 jurassic. (+12)");
+    System.out.println("4 spider man. (livre)");
+    System.out.println("5 a hora do mal. (+18)");
+    opcao=leia.nextInt ();
+
+    if (opcao ==1 && idade>=16){
+        System.out.println("ingresso coprado");
+    } else if ( opcao==2 && idade>=10) {
+        System.out.println("ingresso coprado");
+    } else if ( opcao==3 && idade>=12) {
+        System.out.println("ingresso coprado");
+    } else if ( opcao==4) {
+        System.out.println("ingresso coprado");
+    } else if ( opcao==5 && idade>=18) {
+        System.out.println("ingresso coprado");
+    } else {
+        System.out.println("idade nao permitida");
     }
-}
+
+} while (opcao<1 && opcao>5);
+    } 
+ }
